aa75dfd4237661ada57df9ac6d4d86afa09f1370
omkarbhedercc <ovbhede@redcloudcomputing.com>
2026-04-17 09:54:44 +0530

    feat(programs): add 002 - DISPLAY multiple lines in sequence

---
+++ programs/002-display-multiple-lines/display-multiple.cbl
@@ -0,0 +1,8 @@
+IDENTIFICATION DIVISION.
+PROGRAM-ID. DISPLAY-MULTIPLE.
+
+PROCEDURE DIVISION.
+    DISPLAY "Line 1: First line of output".
+    DISPLAY "Line 2: Second line of output".
+    DISPLAY "Line 3: Third line of output".
+    STOP RUN.
