fad317f276c6eb8d15184d5ef34fcf2d393bb99d
omkarbhedercc <ovbhede@redcloudcomputing.com>
2026-04-20 09:27:41 +0530

    feat(programs): add 007 - fixed-format comments using * in column 7

---
+++ programs/007-fixed-format-comments/fixed-format-comments.cbl
@@ -0,0 +1,11 @@
+      * This is a fixed-format comment at column 7
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. FIXED-FORMAT-COMMENTS.
+      * Another comment before the procedure
+       PROCEDURE DIVISION.
+      * Comment before first DISPLAY
+           DISPLAY "Line one".
+      * Mid-program comment
+           DISPLAY "Line two".
+      * Final comment before STOP
+           STOP RUN.
